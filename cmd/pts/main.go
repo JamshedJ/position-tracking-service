6bc552a01c0dc63a694c26a84a40ebd9fa01a6bd
Jamshed <jamshed@MacBook-Pro-Jamshed.local>
2024-04-16 11:55:25 +0500

    init logger

--- cmd/pts/main.go
+++ cmd/pts/main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/JamshedJ/position-tracking-service/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// TODO: init logger
-
+	log := setupLogger(cfg.Env)
+	log.Info("running application")
+	
 	// TODO: init app
 
 	// TODO: run GRPC server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
